6d6c0745329f97c84f472945ee98a98a0628458c
Blackeye-101 <rishabh.th25@gmail.com>
2022-08-15 11:52:36 +0530

    initial commit

---
+++ canvas tutorial/colorFade.js
@@ -0,0 +1,75 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particleArray = [];
+let hue = 0;
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+canvas.addEventListener("click", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  for (let i = 0; i < 10; i++) {
+    particleArray.push(new Particle());
+  }
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  for (let i = 0; i < 5; i++) {
+    particleArray.push(new Particle());
+  }
+});
+
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * 10 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+    this.color = "hsl(" + hue + ",100%,50%)";
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+const handleParticles = () => {
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+    particleArray[i].draw();
+    if (particleArray[i].size <= 0.3) {
+      particleArray.splice(i, 1);
+      i--;
+    }
+  }
+};
+
+const animate = () => {
+  ctx.fillStyle = "rgba(0,0,0,0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  hue += 0.5;
+  requestAnimationFrame(animate);
+};
+
+animate();
